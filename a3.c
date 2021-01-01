bac79c18748952a508c5df9d5532277ee9aa10c3
matthiasxg <matthiasxgrill@gmail.com>
2021-01-01 15:56:27 +0100

    Print gamefield works

--- a3.c
+++ a3.c
@@ -24,7 +24,8 @@
 #define SIZE_COLUMN_START 1
 #define SIZE_ROW_END 1
 #define SIZE_COLUMN_END 1
-#define SIZE_HIGHSCORE_ENTRIES 1
+#define SIZE_HIGHSCORE_ENTRY 1
+#define SIZE_GAMEFIELD_ENTRY 1
 
 #define SIZE_HIGHSCORE_ENTRY_POINTS 1
 #define SIZE_HIGHSCORE_ENTRY_NAME 3
@@ -56,11 +57,16 @@ bool shouldPipeConnectInDirection(uint8_t **map, uint8_t width, uint8_t height, 
 
 uint8_t *getAdjacentPipe(uint8_t **map, uint8_t width, uint8_t height, uint8_t coord[2], Direction dir);
 
-bool checkConfigFile(char *path);
+bool checkConfigFile(FILE *fp);
 
-void getGameSpecifications(char *path, int *size_field, int *size_start, int *size_end, int *highscore_entries);
+void getGameSpecifications(FILE *fp, uint8_t *size_field, uint8_t *size_start, uint8_t *size_end,
+                           unsigned int *highscore_entries);
 
-void getHighscores(char *path, int highscore_entries, Highscore *highscores);
+void getHighscores(FILE *fp, unsigned int highscore_entries, Highscore *highscores);
+
+bool fillGameMap(uint8_t **map, FILE *fp, uint8_t *size_field);
+
+void freeMap(uint8_t **map, uint8_t rows);
 
 //-----------------------------------------------------------------------------
 ///
@@ -72,35 +78,58 @@ int main(int argc, char *argv[])
 {
   if (--argc == COMMANDLINE_PARAMETER)
   {
-    char *path = argv[1];
-    if (checkConfigFile(path))
+    FILE *fp = fopen(argv[1], "rb");
+    if (fp)
     {
-      int size_field[2] = {0};
-      int size_start[2] = {0};
-      int size_end[2] = {0};
-      int highscore_entries = 0;
-
-      getGameSpecifications(path, size_field, size_start, size_end, &highscore_entries);
-
-      Highscore *highscores = malloc(sizeof(Highscore) * highscore_entries);
-      if (highscores)
+      if (checkConfigFile(fp))
       {
-        getHighscores(path, highscore_entries, highscores);
+        uint8_t size_field[2] = {0};
+        uint8_t size_start[2] = {0};
+        uint8_t size_end[2] = {0};
+        unsigned int highscore_entries = 0;
+        getGameSpecifications(fp, size_field, size_start, size_end, &highscore_entries);
+
+        Highscore *highscores = malloc(sizeof(Highscore) * highscore_entries);
+        if (highscores)
+        {
+          getHighscores(fp, highscore_entries, highscores);
+
+          uint8_t **map = malloc(sizeof(uint8_t *) * size_field[1]);
+          if (map)
+          {
+            if (fillGameMap(map, fp, size_field)) {
+              printMap(map, size_field[0], size_field[1], size_start, size_end);
+            }
+          }
+          else
+          {
+            printf(ERROR_OUT_OF_MEMORY);
+          }
+        }
+        else
+        {
+          printf(ERROR_OUT_OF_MEMORY);
+        }
+
+        /*
+        for (unsigned int i = 0; i < highscore_entries; i++)
+        {
+          printf("Name: %s - Score: %d\n", highscores[i].name, highscores[i].score);
+        }
+
+        printf("Field: %d - %d\n", size_field[0], size_field[1]);
+        printf("Start: %d - %d\n", size_start[0], size_start[1]);
+        printf("End: %d - %d\n", size_end[0], size_end[1]);
+        printf("Highscores: %d\n", highscore_entries);*/
       }
       else
       {
-        printf(ERROR_OUT_OF_MEMORY);
+        printf(ERROR_INVALID_FILE, argv[1]);
       }
-
-      /*for (int i = 0; i < highscore_entries; i++)
-      {
-        printf("Name: %s - Score: %d\n", highscores[i].name, highscores[i].score);
-      }
-
-      printf("Field: %d - %d\n", size_field[0], size_field[1]);
-      printf("Start: %d - %d\n", size_start[0], size_start[1]);
-      printf("End: %d - %d\n", size_end[0], size_end[1]);
-      printf("Highscores: %d\n", highscore_entries);*/
+    }
+    else
+    {
+      printf(ERROR_OPEN_FILE, argv[1]);
     }
   }
   else
@@ -110,85 +139,68 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-void getHighscores(char *path, int highscore_entries, Highscore *highscores)
+bool fillGameMap(uint8_t **map, FILE *fp, uint8_t *size_field)
 {
-  FILE *fp = fopen(path, "rb");
-  if (fp)
+  for (int row = 0; row < size_field[1]; row++)
   {
-    fseek(fp, 14, SEEK_SET);
-
-    for (int index = 0; index < highscore_entries; index++)
-    {
-      fread(&(highscores[index].score), SIZE_HIGHSCORE_ENTRY_POINTS, 1, fp);
-      fread(&(highscores[index].name), SIZE_HIGHSCORE_ENTRY_NAME, 1, fp);
-    }
-
-    fclose(fp);
-  }
-  else
-  {
-    printf(ERROR_OPEN_FILE, path);
-  }
-}
-
-// TODO
-void getGameSpecifications(char *path, int *size_field, int *size_start, int *size_end, int *highscore_entries)
-{
-  FILE *fp = fopen(path, "rb");
-  if (fp)
-  {
-    fseek(fp, SIZE_MAGIC_NUMBER, SEEK_SET);
-
-    fread(size_field, SIZE_FIELD_WIDTH, 1, fp);
-    fread(size_field + 1, SIZE_FIELD_HEIGHT, 1, fp);
-
-    fread(size_start, SIZE_ROW_START, 1, fp);
-    fread(size_start + 1, SIZE_COLUMN_START, 1, fp);
-
-    fread(size_end, SIZE_ROW_END, 1, fp);
-    fread(size_end + 1, SIZE_COLUMN_END, 1, fp);
-
-    fread(highscore_entries, SIZE_HIGHSCORE_ENTRIES, 1, fp);
-
-    fclose(fp);
-  }
-  else
-  {
-    printf(ERROR_OPEN_FILE, path);
-  }
-}
-
-// TODO
-bool checkConfigFile(char *path)
-{
-  FILE *fp = fopen(path, "rb");
-  if (fp)
-  {
-    char *magic_number = malloc(sizeof(char) * SIZE_MAGIC_NUMBER);
-    if (magic_number)
-    {
-      fread(magic_number, sizeof(char), SIZE_MAGIC_NUMBER, fp);
-      if (strcmp(magic_number, MAGIC_NUMBER) != 0)
+    map[row] = malloc(sizeof(uint8_t) * size_field[0]);
+    if (map[row]) {
+      for (int column = 0; column < size_field[0]; column++)
       {
-        printf(ERROR_INVALID_FILE, path);
-        free(magic_number);
-        fclose(fp);
-        return false;
+        fread(&(map[row][column]), SIZE_GAMEFIELD_ENTRY, 1, fp);
       }
-      free(magic_number);
     }
     else
     {
       printf(ERROR_OUT_OF_MEMORY);
-      fclose(fp);
+      freeMap(map, size_field[1]);
       return false;
     }
   }
-  else
+  return true;
+}
+
+void freeMap(uint8_t **map, uint8_t rows) {
+  for (int row = 0; row < rows; row++) {
+    free(map[row]);
+  }
+  free(map);
+}
+
+
+void getHighscores(FILE *fp, unsigned int highscore_entries, Highscore *highscores)
+{
+  for (unsigned int index = 0; index < highscore_entries; index++)
   {
-    printf(ERROR_OPEN_FILE, path);
+    fread(&(highscores[index].score), SIZE_HIGHSCORE_ENTRY_POINTS, 1, fp);
+    fread(&(highscores[index].name), SIZE_HIGHSCORE_ENTRY_NAME, 1, fp);
+  }
+}
+
+// TODO
+void getGameSpecifications(FILE *fp, uint8_t *size_field, uint8_t *size_start, uint8_t *size_end, unsigned int
+*highscore_entries)
+{
+  fread(size_field, SIZE_FIELD_WIDTH, 1, fp);
+  fread(size_field + 1, SIZE_FIELD_HEIGHT, 1, fp);
+
+  fread(size_start, SIZE_ROW_START, 1, fp);
+  fread(size_start + 1, SIZE_COLUMN_START, 1, fp);
+
+  fread(size_end, SIZE_ROW_END, 1, fp);
+  fread(size_end + 1, SIZE_COLUMN_END, 1, fp);
+
+  fread(highscore_entries, SIZE_HIGHSCORE_ENTRY, 1, fp);
+}
+
+// TODO
+bool checkConfigFile(FILE *fp)
+{
+  char magic_number[SIZE_MAGIC_NUMBER + 1] = {0};
+  fread(magic_number, sizeof(char), SIZE_MAGIC_NUMBER, fp);
+  if (strcmp(magic_number, MAGIC_NUMBER) != 0)
+  {
     return false;
   }
-  fclose(fp);
   return true;
 }
