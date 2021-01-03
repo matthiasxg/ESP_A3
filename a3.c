9424995d918553c658a20fbd7f930b95b41bec33
Matthias Grill <matthias.grill@student.tugraz.at>
2021-01-03 14:25:50 +0100

    Add helper function to rotate

--- a3.c
+++ a3.c
@@ -30,6 +30,11 @@
 #define SIZE_HIGHSCORE_ENTRY_POINTS 1
 #define SIZE_HIGHSCORE_ENTRY_NAME 3
 
+#define CHECK_TOP 64
+#define CHECK_LEFT 16
+#define CHECK_BOTTOM 4
+#define CHECK_RIGHT 1
+
 //strings
 #define MAGIC_NUMBER "ESPipes"
 
@@ -50,11 +55,13 @@ typedef struct _Highscore_
 // forward declarations
 bool isDirectionOutOfMap(uint8_t width, uint8_t height, uint8_t coord[2], Direction dir);
 
-bool isPipeOpenInDirection(uint8_t **map, uint8_t width, uint8_t height, uint8_t coord[2], Direction dir);
+bool isPipeOpenInDirection(uint8_t **map, uint8_t coord[2], Direction dir);
 
 bool shouldPipeConnectInDirection(uint8_t **map, uint8_t width, uint8_t height, uint8_t coord[2], Direction dir);
 
 uint8_t *getAdjacentPipe(uint8_t **map, uint8_t width, uint8_t height, uint8_t coord[2], Direction dir);
+
+bool getCoordOfAdjacentPipe(uint8_t width, uint8_t height, uint8_t coord[2], uint8_t adjacent_coord[2], Direction dir);
 
 bool checkConfigFile(FILE *fp);
 
@@ -72,6 +79,8 @@ void startGame(uint8_t **map, uint8_t width, uint8_t height, uint8_t *start_pipe
 void getCommand(Command *cmd, Direction *dir, uint8_t *row, uint8_t *col, unsigned int round);
 
 bool checkRotateValues(uint8_t row, uint8_t col, uint8_t width, uint8_t height, uint8_t *start_pipe, uint8_t *end_pipe);
+
+void rotatePipe(uint8_t **map, uint8_t width, uint8_t height, uint8_t row, uint8_t col, Direction dir);
 
 //-----------------------------------------------------------------------------
 ///
@@ -104,6 +113,7 @@ int main(int argc, char *argv[])
 
           if (fillGameMap(map, fp, width, height))
           {
+            fclose(fp);
             printMap(map, width, height, start_pipe, end_pipe);
             startGame(map, width, height, start_pipe, end_pipe, 1);
           }
@@ -205,7 +215,7 @@ void startGame(uint8_t **map, uint8_t width, uint8_t height, uint8_t *start_pipe
   {
     if (checkRotateValues(row, col, width, height, start_pipe, end_pipe))
     {
-      printf("Correct Values");
+      //rotatePipe(map, width, height, row, col, dir);
     }
     else
     {
@@ -213,6 +223,10 @@ void startGame(uint8_t **map, uint8_t width, uint8_t height, uint8_t *start_pipe
     }
   }
 }
+
+// void rotatePipe(uint8_t **map, uint8_t width, uint8_t height, uint8_t row, uint8_t col, Direction dir)
+// {
+// }
 
 bool checkRotateValues(uint8_t row, uint8_t col, uint8_t width, uint8_t height, uint8_t *start_pipe, uint8_t *end_pipe)
 {
@@ -302,4 +316,166 @@ bool checkConfigFile(FILE *fp)
     return false;
   }
   return true;
+}
+
+bool isDirectionOutOfMap(uint8_t width, uint8_t height, uint8_t coord[2], Direction dir)
+{
+  switch (dir)
+  {
+  case TOP:
+    if (coord[0] == 0)
+    {
+      return true;
+    }
+    break;
+
+  case LEFT:
+    if (coord[1] == 0)
+    {
+      return true;
+    }
+    break;
+
+  case BOTTOM:
+    if (coord[0] == (height - 1))
+    {
+      return true;
+    }
+    break;
+
+  case RIGHT:
+    if (coord[1] == (width - 1))
+    {
+      return true;
+    }
+    break;
+  }
+  return false;
+}
+
+bool isPipeOpenInDirection(uint8_t **map, uint8_t coord[2], Direction dir)
+{
+  uint8_t pipe = map[coord[0]][coord[1]];
+  switch (dir)
+  {
+  case TOP:
+    if ((pipe ^ CHECK_TOP) < pipe)
+    {
+      return true;
+    }
+    break;
+
+  case LEFT:
+    if ((pipe ^ CHECK_LEFT) < pipe)
+    {
+      return true;
+    }
+    break;
+
+  case BOTTOM:
+    if ((pipe ^ CHECK_BOTTOM) < pipe)
+    {
+      return true;
+    }
+    break;
+
+  case RIGHT:
+    if ((pipe ^ CHECK_RIGHT) < pipe)
+    {
+      return true;
+    }
+    break;
+  }
+  return false;
+}
+
+bool shouldPipeConnectInDirection(uint8_t **map, uint8_t width, uint8_t height, uint8_t coord[2], Direction dir)
+{
+  if (isDirectionOutOfMap(width, height, coord, dir))
+  {
+    return false;
+  }
+  if (isPipeOpenInDirection(map, coord, dir))
+  {
+    uint8_t adjacent_coord[2] = {0};
+    if (getCoordOfAdjacentPipe(width, height, coord, adjacent_coord, dir))
+    {
+      Direction adjadent_dir = TOP;
+      if (dir == TOP || dir == LEFT)
+      {
+        adjadent_dir += 2;
+      }
+      else
+      {
+        adjadent_dir -= 2;
+      }
+      if (isPipeOpenInDirection(map, adjacent_coord, adjadent_dir))
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+bool getCoordOfAdjacentPipe(uint8_t width, uint8_t height, uint8_t coord[2], uint8_t adjacent_coord[2], Direction dir)
+{
+  if (!isDirectionOutOfMap(width, height, coord, dir))
+  {
+    switch (dir)
+    {
+    case TOP:
+      adjacent_coord[0] = (coord[0] - 1);
+      adjacent_coord[1] = coord[1];
+      return true;
+      break;
+
+    case LEFT:
+      adjacent_coord[0] = coord[0];
+      adjacent_coord[1] = (coord[1] - 1);
+      return true;
+      break;
+
+    case BOTTOM:
+      adjacent_coord[0] = (coord[0] + 1);
+      adjacent_coord[1] = coord[1];
+      return true;
+      break;
+
+    case RIGHT:
+      adjacent_coord[0] = coord[0];
+      adjacent_coord[1] = (coord[1] + 1);
+      return true;
+      break;
+    }
+  }
+  return false;
+}
+
+uint8_t *getAdjacentPipe(uint8_t **map, uint8_t width, uint8_t height, uint8_t coord[2], Direction dir)
+{
+  if (isDirectionOutOfMap(width, height, coord, dir))
+  {
+    return NULL;
+  }
+
+  switch (dir)
+  {
+  case TOP:
+    return &map[(coord[0] - 1)][coord[1]];
+    break;
+
+  case LEFT:
+    return &map[coord[0]][(coord[1] - 1)];
+    break;
+
+  case BOTTOM:
+    return &map[(coord[0] + 1)][coord[1]];
+    break;
+
+  case RIGHT:
+    return &map[coord[0]][(coord[1] + 1)];
+    break;
+  }
+  return NULL;
 }
